514769d9e28f40909bb4650bf07f58f88c19d962
Yanick Santos <yanickeduardo04@outlook.com>
2022-04-05 15:01:52 -0300

    terminado o javascrip necessario para a secção perguntas frenquentes

--- script.js
+++ script.js
@@ -11,15 +11,19 @@ function Language(){
     }
 }
 
-document.querySelector(".netflix").style.display="none"
+document.querySelector(".netflix1").style.display="none"
 function Netflix(){
-    verificadora = "netflix";
+    verificadora = "netflix1";
     Fechar(verificadora)
-    if(document.querySelector(".netflix").style.display =="none"){
-        document.querySelector(".netflix").style.display="block"
+    if(document.querySelector(".netflix1").style.display =="none"){
+        document.querySelector("#netflix_img").src="img/outline_clear_white_24dp.png"
+        document.querySelector(".netflix1").style.display="block"
+        document.querySelector(".netflix2").style.display="block"
     }
     else{
-        document.querySelector(".netflix").style.display="none"
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector(".netflix1").style.display="none"
+        document.querySelector(".netflix2").style.display="none"
     }
 }
 
@@ -29,23 +33,29 @@ function Custo(){
     verificadora="custo"
     Fechar(verificadora)
     if(document.querySelector(".custo").style.display =="none"){
+        document.querySelector("#custo_img").src="img/outline_clear_white_24dp.png"
         document.querySelector(".custo").style.display="block"
     }
     else{
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
         document.querySelector(".custo").style.display="none"
     }
 }
 
 
-document.querySelector(".onde").style.display="none"
+document.querySelector(".onde1").style.display="none"
 function Onde(){
-    verificadora = "onde"
+    verificadora = "onde1"
     Fechar(verificadora)
-    if(document.querySelector(".onde").style.display =="none"){
-        document.querySelector(".onde").style.display="block"
+    if(document.querySelector(".onde1").style.display =="none"){
+        document.querySelector("#onde_img").src="img/outline_clear_white_24dp.png"
+        document.querySelector(".onde1").style.display="block"
+        document.querySelector(".onde2").style.display="block"
     }
     else{
-        document.querySelector(".onde").style.display="none"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector(".onde1").style.display="none"
+        document.querySelector(".onde2").style.display="none"
     }
 }
 
@@ -55,9 +65,11 @@ function Cancelar(){
     verificadora="cancelar"
     Fechar(verificadora)
     if(document.querySelector(".cancelar").style.display =="none"){
+        document.querySelector("#cancelar_img").src="img/outline_clear_white_24dp.png"
         document.querySelector(".cancelar").style.display="block"
     }
     else{
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
         document.querySelector(".cancelar").style.display="none"
     }
 }
@@ -69,67 +81,124 @@ function Assistir(){
     verificadora="assistir"
     Fechar(verificadora)
     if(document.querySelector(".assistir").style.display =="none"){
+        document.querySelector("#assistir_img").src="img/outline_clear_white_24dp.png"
         document.querySelector(".assistir").style.display="block"
     }
     else{
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
         document.querySelector(".assistir").style.display="none"
     }
 }
 
-document.querySelector(".criancas").style.display="none"
+document.querySelector(".criancas1").style.display="none"
 function Criancas(){
-    verificadora="criancas"
+    verificadora="criancas1"
     Fechar(verificadora)
-    if(document.querySelector(".criancas").style.display =="none"){
-        document.querySelector(".criancas").style.display="block"
+    if(document.querySelector(".criancas1").style.display =="none"){
+        document.querySelector("#criancas_img").src="img/outline_clear_white_24dp.png"
+        document.querySelector(".criancas1").style.display="block"
+        document.querySelector(".criancas2").style.display="block"
     }
     else{
-        document.querySelector(".criancas").style.display="none"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
+        document.querySelector(".criancas1").style.display="none"
+        document.querySelector(".criancas2").style.display="none"
     }
 }
 
 function Fechar(ver){
-    if(ver == "netflix"){
+    if(ver == "netflix1"){
         document.querySelector(".custo").style.display="none";
-        document.querySelector(".onde").style.display="none";
+        document.querySelector(".onde1").style.display="none";
+        document.querySelector(".onde2").style.display="none";
         document.querySelector(".cancelar").style.display="none";
         document.querySelector(".assistir").style.display="none";
-        document.querySelector(".criancas").style.display="none";
+        document.querySelector(".criancas1").style.display="none";
+        document.querySelector(".criancas2").style.display="none"
+
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
     }
     else if(ver == "custo"){
-        document.querySelector(".netflix").style.display="none";
-        document.querySelector(".onde").style.display="none";
+        document.querySelector(".netflix1").style.display="none";
+        document.querySelector(".netflix2").style.display="none";
+        document.querySelector(".onde1").style.display="none";
+        document.querySelector(".onde2").style.display="none"
         document.querySelector(".cancelar").style.display="none";
         document.querySelector(".assistir").style.display="none";
-        document.querySelector(".criancas").style.display="none";
+        document.querySelector(".criancas1").style.display="none";
+        document.querySelector(".criancas2").style.display="none"
+
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
     }
-    else if(ver == "onde"){
-        document.querySelector(".netflix").style.display="none";
+    else if(ver == "onde1"){
+        document.querySelector(".netflix1").style.display="none";
+        document.querySelector(".netflix2").style.display="none";
         document.querySelector(".custo").style.display="none";
         document.querySelector(".cancelar").style.display="none";
         document.querySelector(".assistir").style.display="none";
-        document.querySelector(".criancas").style.display="none";
+        document.querySelector(".criancas1").style.display="none";
+        document.querySelector(".criancas2").style.display="none"
+
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
     }
     else if(ver == "cancelar"){
-        document.querySelector(".netflix").style.display="none";
+        document.querySelector(".netflix1").style.display="none";
+        document.querySelector(".netflix2").style.display="none";
         document.querySelector(".custo").style.display="none";
-        document.querySelector(".onde").style.display="none";
+        document.querySelector(".onde1").style.display="none";
+        document.querySelector(".onde2").style.display="none";
         document.querySelector(".assistir").style.display="none";
-        document.querySelector(".criancas").style.display="none";
+        document.querySelector(".criancas1").style.display="none";
+        document.querySelector(".criancas2").style.display="none"
+
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
     }
     else if(ver == "assistir"){
-        document.querySelector(".netflix").style.display="none";
+        document.querySelector(".netflix1").style.display="none";
+        document.querySelector(".netflix2").style.display="none";
         document.querySelector(".custo").style.display="none";
-        document.querySelector(".onde").style.display="none";
+        document.querySelector(".onde1").style.display="none";
+        document.querySelector(".onde2").style.display="none";
         document.querySelector(".cancelar").style.display="none";
-        document.querySelector(".criancas").style.display="none";
+        document.querySelector(".criancas1").style.display="none";
+        document.querySelector(".criancas2").style.display="none"
+
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#criancas_img").src="img/outline_add_white_24dp.png"
     }
     else{
-        document.querySelector(".netflix").style.display="none";
+        document.querySelector(".netflix1").style.display="none";
+        document.querySelector(".netflix2").style.display="none";
         document.querySelector(".custo").style.display="none";
-        document.querySelector(".onde").style.display="none";
+        document.querySelector(".onde1").style.display="none";
+        document.querySelector(".onde2").style.display="none";
         document.querySelector(".cancelar").style.display="none";
         document.querySelector(".assistir").style.display="none";
+
+        document.querySelector("#netflix_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#custo_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#onde_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#cancelar_img").src="img/outline_add_white_24dp.png"
+        document.querySelector("#assistir_img").src="img/outline_add_white_24dp.png"
     }
 
 }
